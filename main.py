802d4b41bf454107468a728279c0bf662ae00e25
maksart72 <maksim.artemyev@gmail.com>
2017-08-25 18:19:41 +0500

    Draft

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import markdown
+import json
 
 
+def load_article():
+    html = markdown.markdown('# Hello!')
+    return html
 
-def load_article(filename)
+def load_sitemap():
+    with open('config.json','r', encoding='utf-8') as config:
+        sitemap = json.load(config)
+    return sitemap
 
-    html = markdown.markdown(your_text_string)
-
-return html
+if __name__ == '__main__':
+    print(load_sitemap())
--- run.py
+++ run.py
@@ -1,16 +1,31 @@
+import markdown
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
 
+
+
+def load_article():
+    with open('articles/1_python_basics/1_intro.md', 'r', encoding='utf-8') as file:
+        html = markdown.markdown(file.read())
+    return html
+
+def load_sitemap():
+    with open('config.json','r', encoding='utf-8') as config:
+        sitemap = json.load(config)
+    return sitemap
+
+@app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/view")
 
+@app.route("/view")
 def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    article = load_article()
+    return render_template('template.html', article = article)
 
 
 if __name__ == '__main__':
