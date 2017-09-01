592299a94771e6512a3f746256d7fcb5d2d9f5a9
maksart72 <maksim.artemyev@gmail.com>
2017-09-01 18:50:23 +0500

    Flask, generate pages from config.json

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 import markdown
 import json
+import collections
 
 
-def load_article():
-    html = markdown.markdown('# Hello!')
-    return html
+source = '1_python_basics'
+
+def load_article(source):
+    Article = collections.namedtuple('Article', 'text title slug')
+    with open('articles/'+source, 'r', encoding='utf-8') as file:
+        html = markdown.markdown(file.read())
+
+    json_sitemap = load_sitemap()
+    for top in json_sitemap['articles']:
+        if top['source'] == source:
+            title = top['title']
+            topic = top['topic']
+
+    content = Article(text=html, title=title, slug=topic)
+    return content
+
+
+
 
 def load_sitemap():
     with open('config.json','r', encoding='utf-8') as config:
         sitemap = json.load(config)
     return sitemap
 
+def index():
+
+    json_sitemap = load_sitemap()
+    sitemap = []
+    source = '1_python_basics'
+    for url in json_sitemap['articles']:
+        article_info = (url['source'].split('/')[0],url['source'].split('/')[1].split('.')[0], url['title'])
+        sitemap.append(article_info)
+
+
+
+    category_list = json_sitemap['topics']
+    for slug in category_list:
+        print(str(source.split('_')[1]))
+    #   if slug['slug'] == str(source.split('_')[1]):
+    #       category = slug['title']
+    return(None)
+
 if __name__ == '__main__':
+    source = '0_tutorial/14_google.md'
+    article = load_article(source)
+    print(article)
     print(load_sitemap())
+    print(index())
+
--- run.py
+++ run.py
@@ -1,32 +1,70 @@
-import markdown
 import json
-from flask import Flask, render_template
+import collections
+from livereload import Server
+import markdown
+from flask import (
+    Flask,
+    render_template)
 app = Flask(__name__)
 
+CONFIG_PATH = "config.json"
 
+def load_article(article_url):
 
+    json_sitemap = load_sitemap()
+    for top in json_sitemap['articles']:
+        if top['source'].split('/')[1].split('.')[0] == article_url:
+            title = top['title']
+            topic = top['topic']
+            source = top['source']
 
-
-def load_article():
-    with open('articles/1_python_basics/1_intro.md', 'r', encoding='utf-8') as file:
+    Article = collections.namedtuple('Article', 'text title slug')
+    with open('articles/'+source, 'r', encoding='utf-8') as file:
         html = markdown.markdown(file.read())
-    return html
+
+    content = Article(text=html, title=title, slug=topic)
+    return content
+
 
 def load_sitemap():
-    with open('config.json','r', encoding='utf-8') as config:
+    with open(CONFIG_PATH,'r', encoding='utf-8') as config:
         sitemap = json.load(config)
     return sitemap
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    json_sitemap = load_sitemap()
+    sitemap = []
+    for url in json_sitemap['articles']:
+        article_info = (url['topic'],url['source'].split('/')[1].split('.')[0], url['title'])
+        sitemap.append(article_info)
+    return render_template("index.html",
+                       articles=sitemap)
 
 
-@app.route("/view")
-def template_test():
-    article = load_article()
-    return render_template('template.html', article = article)
+@app.route('/<topic>/')
+def view_topics(topic):
+    json_sitemap = load_sitemap()
+    sitemap = []
+    for url in json_sitemap['articles']:
+        if url['topic'] == topic:
+            article_info = (url['topic'],url['source'].split('/')[1].split('.')[0], url['title'])
+            sitemap.append(article_info)
 
+    category_list = json_sitemap['topics']
+    for slug in category_list:
+        if slug['slug'] == topic:
+            category = slug['title']
+
+    return render_template("category.html",
+                       articles=sitemap, category = category)
+
+@app.route('/<topic>/<article_url>')
+
+def view_article(topic=None, article_url=None):
+    article = load_article(article_url)
+    return render_template('article.html', article = article.text, title=article.title, slug=article.slug)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
